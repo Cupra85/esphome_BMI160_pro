6e211338c291352b7973ababc7d6c7f6c4ee9d0e
Cupra85 <daniel.curten@gmail.com>
2025-11-27 17:11:51 +0100

    Update sensor.py

--- components/bmi160_pro/sensor.py
+++ components/bmi160_pro/sensor.py
@@ -1,148 +1,56 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-
 from esphome.components import i2c, sensor
-from esphome.const import (
-    CONF_ID,
-    CONF_ADDRESS,
-    CONF_UPDATE_INTERVAL,
-    UNIT_METER_PER_SECOND_SQUARED,
-    UNIT_CELSIUS,
-    ICON_ACCELERATION,
-    ICON_THERMOMETER,
-    ICON_ROTATE_3D,
-    STATE_CLASS_MEASUREMENT,
-)
+from esphome.const import CONF_ID
 
-# ----- Metadaten -----
-MULTI_CONF = True         # mehrere Instanzen erlaubt
-DEPENDENCIES = ["i2c"]    # benötigt I2C
-
-# C++ Namespace + Klasse
+# Namespace für C++
 bmi160_pro_ns = cg.esphome_ns.namespace("bmi160_pro")
 BMI160Pro = bmi160_pro_ns.class_("BMI160Pro", cg.PollingComponent, i2c.I2CDevice)
 
-# ---- eigene YAML-Keys ----
-CONF_ACCEL_X = "accel_x"
-CONF_ACCEL_Y = "accel_y"
-CONF_ACCEL_Z = "accel_z"
-CONF_GYRO_X = "gyro_x"
-CONF_GYRO_Y = "gyro_y"
-CONF_GYRO_Z = "gyro_z"
-CONF_PITCH = "pitch"
-CONF_ROLL = "roll"
-CONF_INCLINATION = "inclination"
-CONF_TEMPERATURE = "temperature"
-CONF_VIBRATION = "vibration"
+# Standard-Schema (kompatibel mit allen ESPHome-Versionen)
+SIMPLE = sensor.sensor_schema()
 
-CONF_TILT_THRESHOLD = "tilt_threshold_deg"
-CONF_MOTION_THRESHOLD = "motion_threshold_ms2"
-CONF_VIBRATION_THRESHOLD = "vibration_threshold_ms2"
-
-# ----- Sensor-Schemas -----
-accel_schema = sensor.sensor_schema(
-    unit_of_measurement=UNIT_METER_PER_SECOND_SQUARED,
-    icon=ICON_ACCELERATION,
-    accuracy_decimals=3,
-    state_class=STATE_CLASS_MEASUREMENT,
-)
-
-gyro_schema = sensor.sensor_schema(
-    unit_of_measurement="°/s",
-    icon=ICON_ROTATE_3D,
-    accuracy_decimals=3,
-    state_class=STATE_CLASS_MEASUREMENT,
-)
-
-angle_schema = sensor.sensor_schema(
-    unit_of_measurement="°",
-    icon=ICON_ROTATE_3D,
-    accuracy_decimals=1,
-    state_class=STATE_CLASS_MEASUREMENT,
-)
-
-temp_schema = sensor.sensor_schema(
-    unit_of_measurement=UNIT_CELSIUS,
-    icon=ICON_THERMOMETER,
-    accuracy_decimals=1,
-    state_class=STATE_CLASS_MEASUREMENT,
-)
-
-vibration_schema = sensor.sensor_schema(
-    unit_of_measurement=UNIT_METER_PER_SECOND_SQUARED,
-    icon=ICON_ACCELERATION,
-    accuracy_decimals=3,
-    state_class=STATE_CLASS_MEASUREMENT,
-)
-
-# ----- CONFIG_SCHEMA -----
 CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(CONF_ID): cv.declare_id(BMI160Pro),
 
-            # optionale Sensor-Ausgänge:
-            cv.Optional(CONF_ACCEL_X): accel_schema,
-            cv.Optional(CONF_ACCEL_Y): accel_schema,
-            cv.Optional(CONF_ACCEL_Z): accel_schema,
+            cv.Optional("accel_x"): SIMPLE,
+            cv.Optional("accel_y"): SIMPLE,
+            cv.Optional("accel_z"): SIMPLE,
 
-            cv.Optional(CONF_GYRO_X): gyro_schema,
-            cv.Optional(CONF_GYRO_Y): gyro_schema,
-            cv.Optional(CONF_GYRO_Z): gyro_schema,
+            cv.Optional("gyro_x"): SIMPLE,
+            cv.Optional("gyro_y"): SIMPLE,
+            cv.Optional("gyro_z"): SIMPLE,
 
-            cv.Optional(CONF_PITCH): angle_schema,
-            cv.Optional(CONF_ROLL): angle_schema,
-            cv.Optional(CONF_INCLINATION): angle_schema,
+            cv.Optional("pitch"): SIMPLE,
+            cv.Optional("roll"): SIMPLE,
+            cv.Optional("inclination"): SIMPLE,
 
-            cv.Optional(CONF_TEMPERATURE): temp_schema,
-            cv.Optional(CONF_VIBRATION): vibration_schema,
+            cv.Optional("temperature"): SIMPLE,
+            cv.Optional("vibration"): SIMPLE,
 
-            # Schwellenwerte für C++-Seite
-            cv.Optional(CONF_TILT_THRESHOLD, default=15.0): cv.float_range(min=0.0, max=90.0),
-            cv.Optional(CONF_MOTION_THRESHOLD, default=0.3): cv.float_range(min=0.0, max=10.0),
-            cv.Optional(CONF_VIBRATION_THRESHOLD, default=0.5): cv.float_range(min=0.0, max=50.0),
+            cv.Optional("tilt_threshold_deg", default=15.0): cv.float_,
+            cv.Optional("motion_threshold_ms2", default=0.3): cv.float_,
+            cv.Optional("vibration_threshold_ms2", default=0.5): cv.float_,
         }
     )
-    .extend(i2c.i2c_device_schema(0x68))       # I2C Adresse
-    .extend(cv.polling_component_schema("5s")) # update_interval
+    .extend(i2c.i2c_device_schema(0x68))
+    .extend(cv.polling_component_schema("5s"))
 )
 
-# ----- Code-Generator -----
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
-    # registrieren als Komponente & I2C-Device
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
-    # Schwellenwerte speichern
-    cg.add(var.set_tilt_threshold_deg(config[CONF_TILT_THRESHOLD]))
-    cg.add(var.set_motion_threshold_ms2(config[CONF_MOTION_THRESHOLD]))
-    cg.add(var.set_vibration_threshold_ms2(config[CONF_VIBRATION_THRESHOLD]))
+    # Thresholds an Objekt schreiben
+    cg.add(var.set_tilt_threshold_deg(config["tilt_threshold_deg"]))
+    cg.add(var.set_motion_threshold_ms2(config["motion_threshold_ms2"]))
+    cg.add(var.set_vibration_threshold_ms2(config["vibration_threshold_ms2"]))
 
-    # Sensor-Objekte übergeben
-    if CONF_ACCEL_X in config:
-        cg.add(var.set_accel_x_sensor(await sensor.new_sensor(config[CONF_ACCEL_X])))
-    if CONF_ACCEL_Y in config:
-        cg.add(var.set_accel_y_sensor(await sensor.new_sensor(config[CONF_ACCEL_Y])))
-    if CONF_ACCEL_Z in config:
-        cg.add(var.set_accel_z_sensor(await sensor.new_sensor(config[CONF_ACCEL_Z])))
-
-    if CONF_GYRO_X in config:
-        cg.add(var.set_gyro_x_sensor(await sensor.new_sensor(config[CONF_GYRO_X])))
-    if CONF_GYRO_Y in config:
-        cg.add(var.set_gyro_y_sensor(await sensor.new_sensor(config[CONF_GYRO_Y])))
-    if CONF_GYRO_Z in config:
-        cg.add(var.set_gyro_z_sensor(await sensor.new_sensor(config[CONF_GYRO_Z])))
-
-    if CONF_PITCH in config:
-        cg.add(var.set_pitch_sensor(await sensor.new_sensor(config[CONF_PITCH])))
-    if CONF_ROLL in config:
-        cg.add(var.set_roll_sensor(await sensor.new_sensor(config[CONF_ROLL])))
-    if CONF_INCLINATION in config:
-        cg.add(var.set_inclination_sensor(await sensor.new_sensor(config[CONF_INCLINATION])))
-
-    if CONF_TEMPERATURE in config:
-        cg.add(var.set_temperature_sensor(await sensor.new_sensor(config[CONF_TEMPERATURE])))
-    if CONF_VIBRATION in config:
-        cg.add(var.set_vibration_sensor(await sensor.new_sensor(config[CONF_VIBRATION])))
+    # Sensor-Objekte binden
+    for key in config:
+        if key not in ["id", "tilt_threshold_deg", "motion_threshold_ms2", "vibration_threshold_ms2"]:
+            sens = await sensor.new_sensor(config[key])
+            cg.add(getattr(var, f"set_{key}_sensor")(sens))
